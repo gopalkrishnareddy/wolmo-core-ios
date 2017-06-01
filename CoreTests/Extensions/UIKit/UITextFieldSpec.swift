85a596e666345014c067958a5d7f1535bddf2eaa
danielaRiesgo <danielap.riesgo@gmail.com>
2017-06-01 15:33:07 -0300

    Added tests

--- CoreTests/Extensions/UIKit/UITextFieldSpec.swift
+++ CoreTests/Extensions/UIKit/UITextFieldSpec.swift
@@ -55,9 +55,80 @@ public class UITextFieldSpec: QuickSpec {
                         expect(textField.nextTextField).toNot(beNil())
                         expect(textField.nextTextField!).to(equal(otherTextField))
                     }
+
                 }
+
             }
             
         }
+
+        describe("#fontTextStyle") {
+
+            var textField: UITextField!
+
+            beforeEach {
+                textField = UITextField()
+            }
+
+            describe("get") {
+
+                context("When a style was set") {
+
+                    beforeEach {
+                        textField.fontTextStyle = .body
+                    }
+
+                    it("should return that style") {
+                        expect(textField.fontTextStyle).to(equal(UIFontTextStyle.body))
+                    }
+                    
+                }
+
+                context("When no style was set") {
+
+                    it("should return .none") {
+                        expect(textField.fontTextStyle).to(beNil())
+                    }
+                    
+                }
+
+                context("When a style was set but then font property was changed") {
+
+                    beforeEach {
+                        textField.fontTextStyle = .body
+                        textField.font = UIFont.systemFont(ofSize: 30)
+                    }
+
+                    it("should return that style") {
+                        expect(textField.fontTextStyle).to(beNil())
+                    }
+                    
+                }
+
+            }
+
+            describe("set") {
+
+                beforeEach {
+                    textField.fontTextStyle = .body
+                }
+
+                context("When a style is set") {
+
+                    it("should change the fontTextStyle") {
+                        expect(textField.fontTextStyle).to(equal(UIFontTextStyle.body))
+                    }
+
+                    it("should change the font as well") {
+                        expect(textField.font).to(equal(UIFont.preferredFont(forTextStyle: .body)))
+                    }
+
+                }
+
+            }
+
+        }
+
     }
+
 }
